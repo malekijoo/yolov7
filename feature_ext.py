1dad1bf9829e566cd8953ab7a332482940d1af2c
amir <amirhossein.maleki1990@gmail.com>
2022-11-24 19:53:12 +0330

    adding the feature_ext.py

---
+++ feature_ext.py
@@ -0,0 +1,11 @@
+"""
+This is the file I added.
+Here we are going to receive the prediction of Yolov7.
+Then it calls our feature extraction and save features
+besides the oreder of images and its path.
+
+
+"""
+import numpy as np
+
+print('hello world!')
