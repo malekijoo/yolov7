ce7342d4f0c418404795c06fd82caed0792c0bd1
amir <amirhossein.maleki1990@gmail.com>
2022-11-27 20:12:30 +0330

    adding the feature_ext.py

--- feature_ext.py
+++ feature_ext.py
@@ -6,6 +6,35 @@ besides the oreder of images and its path.
 
 
 """
-import numpy as np
+import os
+import torch
+import math
+import pandas as pd
+from pathlib import Path
 
-print('hello world!')
+class SavingPredictons:
+    def __init__(self, saving_path):
+        self.keys = ('x1', 'y1', 'x2', 'y2', 'conf', 'cls')
+        self.dict_list = []
+        self.saving_path = saving_path.resolve()
+        if not os.path.exists(self.saving_path):
+            os.makedirs(self.saving_path)
+
+    def __call__(self, prediction, path):
+        _dict = dict.fromkeys(self.keys)
+        for sub, val in zip(_dict, prediction.tolist()):
+            if sub in ['x1', 'y1']:
+                _dict[sub] = math.floor(val)
+            elif sub in ['x2', 'y2']:
+                _dict[sub] = math.ceil(val)
+            else:
+                _dict[sub] = val
+
+        _dict['path'] = path
+        self.dict_list.append(_dict)
+
+    def save_to_hdf5(self):
+        # Export the pandas DataFrame into HDF5
+        df_final = pd.DataFrame.from_dict(self.dict_list)
+        h5File = "hdf5_predictions.h5"
+        df_final.to_hdf(Path(self.saving_path, h5File), key='df_final', data_columns=True)
